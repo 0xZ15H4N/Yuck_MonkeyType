71c30096f5ad6b535344688ab1c98b35b8b7f01f
zishan <zishanza436@gmail.com>
2025-04-22 01:01:04 +0530

    Updated

---
+++ README.md
@@ -0,0 +1,14 @@
+To start with first clone the repo and then run 
+
+`pip3 install -r requirements.txt` 
+
+also `Install the appropriate ChromeDriver`, cause selenium only works with ChromeDriver :) 
+
+here is the link : `https://googlechromelabs.github.io/chrome-for-testing/`
+
+make sure you donwload chromedriver and not chrome !
+
+also add the location of chrome from the zip you downloads to the code !
+
+now run and enjoy destroying the monkeyType but make sure you use Fake account cause they ban fast!
+
---
+++ type_with_py.py
@@ -0,0 +1,59 @@
+import pyautogui
+import time
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options  
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+
+########Add Chrome absolute path here #############👇
+service = Service(r"C:\Users\dell\Desktop\temp\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+driver.get("https://monkeytype.com")
+
+
+
+################################################
+###### This Pause is To setup you account ######
+################################################
+
+a = input("Press Enter to continue")
+page_source = driver.page_source
+soup = BeautifulSoup(page_source, "html.parser")
+words = [] # this will contains words
+about_to_type_words = soup.find_all("div",class_="word")
+
+for i in about_to_type_words:
+    word = ''.join([l.get_text() for l in i.find_all('letter')])
+    words.append(word)
+
+print(words)
+
+idx = []
+
+for i in range(int(len(words)*0.1)):
+    idx.append(random.randrange(65,65+25))
+
+
+for i in idx:
+    word = words.pop(int(i/10))
+    word = word[:-1] + chr(i)
+    print("new word is ",word)
+    words.insert(int(i/10),word)
+    
+print(words)
+time.sleep(5)
+for i in words:
+    pyautogui.write(i)
+    if(random.randrange(0,15) == 8):
+        print("inside random")
+        time.sleep(random.randrange(0,15)/10)
+    pyautogui.press("space")
+
+#################################################################
+####### 5 sec window to set the  mouse focus to MonkeyType ######
+#################################################################
+
+time.sleep(5)
+a = input("press Enter to quit chrome!")
